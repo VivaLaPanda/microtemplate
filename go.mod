17c9ae1e6cd9c94a115c04209e47235955c14780
Adrian Smith (VivaLaPanda) <adrian@smithdev.io>
2021-05-27 12:36:24 -0700

    Misc QoL changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/VivaLaPanda/microtemplate
 
 go 1.16
+
+require github.com/fsnotify/fsnotify v1.4.9 // indirect
--- main.go
+++ main.go
@@ -9,18 +9,26 @@ import (
 	"path/filepath"
 	"strings"
 	"text/template"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 // Various runtime flags
 var rootPath = flag.String("path", "./templates", "Where to search for files and templates")
+var watch = flag.Bool("watch", false, "Whether to automatically rebuild on changes")
+var templatePrefix = flag.String("tprefix", "t_", "prefix for template files")
 var outDir = flag.String("out", "./build/", "Where to search for files and templates")
 
 var templates *template.Template
 var absRootPath string
 
 func applyTemplate(currentPath string, d os.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+
 	if strings.Contains(currentPath, path.Join(*rootPath, *outDir)) {
-		// We are in the templates or output folder, so we can just skip
+		// We are in the output folder, so we can just skip
 		return nil
 	}
 
@@ -30,6 +38,11 @@ func applyTemplate(currentPath string, d os.DirEntry, err error) error {
 	// If we're looking at a dir just make the path
 	os.MkdirAll(filepath.Dir(outPath), os.FileMode(os.O_RDWR))
 	if d.IsDir() {
+		return nil
+	}
+
+	// If we're looking at a file and it's marked as a template, don't output it
+	if strings.Contains(filepath.Base(currentPath), *templatePrefix) {
 		return nil
 	}
 
@@ -46,22 +59,74 @@ func applyTemplate(currentPath string, d os.DirEntry, err error) error {
 	return err
 }
 
-func main() {
-	flag.Parse()
-
+func compileTemplates() error {
 	var err error
 	absRootPath, err = filepath.Abs(*rootPath)
 	if err != nil {
-		log.Fatalf("Failed to ref rootPath: %v\n", err)
+		return fmt.Errorf("failed to ref rootPath: %v", err)
 	}
 
 	templates, err = template.ParseGlob(filepath.Join(absRootPath, "*.html"))
 	if err != nil {
-		log.Fatalf("Failed to load templates. 'path' glob may be wrong: %v\n", err)
+		return fmt.Errorf("failed to load templates. 'path' glob may be wrong: %v", err)
 	}
 
 	err = filepath.WalkDir(absRootPath, applyTemplate)
 	if err != nil {
-		log.Fatalf("Failed to apply templates. Err: %v\n", err)
+		return fmt.Errorf("failed to apply templates. Err: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	err := compileTemplates()
+	if err != nil {
+		log.Fatalf("Couldn't compile templates: %v", err)
+	}
+
+	if *watch {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatalf("Failed to make watcher: %v", err)
+		}
+		defer watcher.Close()
+
+		//
+		done := make(chan bool)
+
+		//
+		go func() {
+			for {
+				select {
+				// watch for events
+				case event, ok := <-watcher.Events:
+					if !ok {
+						return
+					}
+
+					if event.Op&fsnotify.Write == fsnotify.Write {
+						log.Println("Modified file:", event.Name)
+					}
+					err := compileTemplates()
+					if err != nil {
+						log.Printf("Couldn't compile templates: %v", err)
+					}
+
+					// watch for errors
+				case err := <-watcher.Errors:
+					log.Printf("Error while watching files: %v\n", err)
+				}
+			}
+		}()
+
+		// out of the box fsnotify can watch a single file, or a single directory
+		if err := watcher.Add(*rootPath); err != nil {
+			log.Fatalf("Failed to start watching path %s: %v", *rootPath, err)
+		}
+
+		<-done
 	}
 }
